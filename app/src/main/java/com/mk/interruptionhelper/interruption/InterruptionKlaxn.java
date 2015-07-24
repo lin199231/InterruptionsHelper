e03a80e550023ae524850835fdce3452af8e6615
lin199231 <lin199231@gmail.com>
2015-07-24 15:06:45 +0800

    add InterruptionKlaxn

---
+++ app/src/main/java/com/mk/interruptionhelper/interruption/InterruptionKlaxn.java
@@ -0,0 +1,138 @@
+package com.mk.interruptionhelper.interruption;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnErrorListener;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Vibrator;
+
+import com.mk.interruptionhelper.LogUtils;
+import com.mk.interruptionhelper.R;
+import com.mk.interruptionhelper.provider.InterruptionInstance;
+
+import java.io.IOException;
+
+/**
+ * Created by dhdev_000 on 2015/7/24.
+ */
+public class InterruptionKlaxn {
+    private static final long[] sVibratePattern = new long[] { 500, 500 };
+
+    // Volume suggested by media team for in-call alarms.
+    private static final float IN_CALL_VOLUME = 0.125f;
+
+    private static final AudioAttributes VIBRATION_ATTRIBUTES = new AudioAttributes.Builder()
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .setUsage(AudioAttributes.USAGE_ALARM)
+            .build();
+
+    private static boolean sStarted = false;
+    private static MediaPlayer sMediaPlayer = null;
+
+    public static void stop(Context context) {
+        LogUtils.v("InterruptionKlaxon.stop()");
+
+        if (sStarted) {
+            sStarted = false;
+            // Stop audio playing
+            if (sMediaPlayer != null) {
+                sMediaPlayer.stop();
+                AudioManager audioManager = (AudioManager)
+                        context.getSystemService(Context.AUDIO_SERVICE);
+                audioManager.abandonAudioFocus(null);
+                sMediaPlayer.release();
+                sMediaPlayer = null;
+            }
+
+            ((Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE)).cancel();
+        }
+    }
+
+    public static void start(final Context context, InterruptionInstance instance,
+                             boolean inTelephoneCall) {
+        LogUtils.v("AlarmKlaxon.start()");
+        // Make sure we are stop before starting
+        stop(context);
+
+        if (!InterruptionInstance.NO_NOTIFICATION_URI.equals(instance.mNotification)) {
+            Uri interruptionNoise = instance.mNotification;
+            // Fall back on the default interruption if the database does not have an
+            // interruption stored.
+            if (interruptionNoise == null) {
+                interruptionNoise = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                LogUtils.v("Using default interruption: " + interruptionNoise.toString());
+            }
+
+            // TODO: Reuse mMediaPlayer instead of creating a new one and/or use RingtoneManager.
+            sMediaPlayer = new MediaPlayer();
+            sMediaPlayer.setOnErrorListener(new OnErrorListener() {
+                @Override
+                public boolean onError(MediaPlayer mp, int what, int extra) {
+                    LogUtils.e("Error occurred while playing audio. Stopping AlarmKlaxon.");
+                    InterruptionKlaxn.stop(context);
+                    return true;
+                }
+            });
+
+            try {
+                // Check if we are in a call. If we are, use the in-call alarm
+                // resource at a low volume to not disrupt the call.
+                if (inTelephoneCall) {
+                    LogUtils.v("Using the in-call interruption");
+                    sMediaPlayer.setVolume(IN_CALL_VOLUME, IN_CALL_VOLUME);
+                    setDataSourceFromResource(context, sMediaPlayer, R.raw.in_call_alarm);
+                } else {
+                    sMediaPlayer.setDataSource(context, interruptionNoise);
+                }
+                startInterruption(context, sMediaPlayer);
+            } catch (Exception ex) {
+                LogUtils.v("Using the fallback ringtone");
+                // The alarmNoise may be on the sd card which could be busy right
+                // now. Use the fallback ringtone.
+                try {
+                    // Must reset the media player to clear the error state.
+                    sMediaPlayer.reset();
+                    setDataSourceFromResource(context, sMediaPlayer, R.raw.fallbackring);
+                    startInterruption(context, sMediaPlayer);
+                } catch (Exception ex2) {
+                    // At this point we just don't play anything.
+                    LogUtils.e("Failed to play fallback ringtone", ex2);
+                }
+            }
+        }
+
+        if (instance.mVibrate) {
+            Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(sVibratePattern, 0, VIBRATION_ATTRIBUTES);
+        }
+
+        sStarted = true;
+    }
+
+    // Do the common stuff when starting the interruption.
+    private static void startInterruption(Context context, MediaPlayer player) throws IOException {
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        // do not play alarms if stream volume is 0 (typically because ringer mode is silent).
+        if (audioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION) != 0) {
+            player.setAudioStreamType(AudioManager.STREAM_NOTIFICATION);
+            player.setLooping(false);
+            player.prepare();
+            audioManager.requestAudioFocus(null,
+                    AudioManager.STREAM_NOTIFICATION, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+            player.start();
+        }
+    }
+
+    private static void setDataSourceFromResource(Context context, MediaPlayer player, int res)
+            throws IOException {
+        AssetFileDescriptor afd = context.getResources().openRawResourceFd(res);
+        if (afd != null) {
+            player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            afd.close();
+        }
+    }
+}
